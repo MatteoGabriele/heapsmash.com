1f4bf8fe641e4308a7ad9e6ab092eb087a567b49
Matteo Gabriele <m.gabriele.dev@gmail.com>
2025-06-30 09:08:05 +0200

    feat: new logo icon

--- components/Logo.vue
+++ components/Logo.vue
@@ -5,7 +5,7 @@
     class="group text-lg font-logo text-neutral-100 flex items-center gap-1"
     to="/"
   >
-    <Icon class="text-xl" name="mdi:buffer" />
-    Heapsmash
+    <Icon class="text-3xl" name="game-icons:hammer-break" />
+    <span> Heap<span class="font-bold">smash</span> </span>
   </NuxtLink>
 </template>
